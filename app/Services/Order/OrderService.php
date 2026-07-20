edcb8eabf704ab3c25e895d7d148710b4d9ff547
mangges <komangbaguslanang85@gmail.com>
2026-07-20 23:51:34 +0800

    fix add order service

---
+++ app/Services/Order/OrderService.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Services\Order;
+
+use App\Models\Order;
+use App\Models\Payment;
+use App\Services\Cart\CartCalculatorService;
+use Illuminate\Support\Facades\Auth;
+use App\Enum\Orders\OrderStatus;
+use App\Enum\Orders\PaymentStatus;
+
+class OrderService
+{
+    public function __construct(
+        private CartCalculatorService $cartCalculatorService
+    ) {}
+
+    public function processOrder(array $cartItems, ?int $tableId = null, ?string $customerName = null, ?string $orderType = null): Order
+    {
+        $cartSubtotal = $this->cartCalculatorService->subtotal($cartItems);
+        $cartTax = $this->cartCalculatorService->tax($cartSubtotal);
+        $totalAmount = $this->cartCalculatorService->total($cartSubtotal, $cartTax);
+        $order =  Order::create([
+            'order_number' => $this->createOrderNumber(),
+            'table_id' => $tableId,
+            'customer_id' => null,
+            'customer_name' => $customerName,
+            'user_id' => Auth::id(),
+            'total_amount' => $totalAmount,
+            'tax' => $cartTax,
+            'discount' => 0,
+            'status' => OrderStatus::Pending,
+            'payment_id' => null,
+            'order_type' => $orderType,
+        ]);
+
+        $payment = Payment::create([
+            'order_id' => $order->id,
+            'payment_method' => 'cash',
+            'amount' => 0,
+            'status' => PaymentStatus::Pending,
+            'transaction_id' => null,
+        ]);
+
+        $order->update([
+            'payment_id' => $payment->id,
+        ]);
+
+        return $order;
+    }
+
+    public function finalizeOrder(?int $orderId, string $paymentMethod, ?float $cashReceived = null, ?string $orderType = null): Order
+    {
+        $order = Order::find($orderId);
+        $payment = $order->payment;
+
+        if ($paymentMethod === 'cash' && $cashReceived < $order->total_amount) {
+            throw new \Exception('Cash received is less than the total amount.');
+        }
+        $order->update([
+            'order_status' => OrderStatus::Completed,
+            'payment_id' => $payment->id,
+            'payment_method' => $paymentMethod,
+            'order_type' => $orderType,
+        ]);
+
+        $payment->update([
+            'payment_method' => $paymentMethod,
+            'amount' => $paymentMethod === 'cash' ? $cashReceived : 0,
+            'status' => PaymentStatus::Success,
+        ]);
+
+        return $order;
+    }
+
+    private function createOrderNumber(): string
+    {
+        $lastOrder = Order::latest()->first();
+        $lastOrderNumber = $lastOrder ? (int) substr($lastOrder->order_number, 3) : 0;
+        $newOrderNumber = str_pad($lastOrderNumber + 1, 6, '0', STR_PAD_LEFT);
+        return 'ORD' . $newOrderNumber;
+    }
+}
