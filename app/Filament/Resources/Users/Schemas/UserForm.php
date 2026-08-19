ebaad46f6e0a48b4f0898d5613317e74af8ec54c
lanangasmara <komangbaguslanang85@gmail.com>
2026-08-19 11:24:21 +0800

    feat: add Filament resource for managing users
    Auto-generated CRUD (list, create, edit) for the User model.

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+                TextInput::make('pin')
+                    ->default(null),
+                Select::make('role')
+                    ->options(['admin' => 'Admin', 'cashier' => 'Cashier'])
+                    ->default('cashier')
+                    ->required(),
+            ]);
+    }
+}
